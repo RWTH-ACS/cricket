0edd15196c1f41de423932c67cfc1253a933b35b
laura fuentes grau <grau@ghost.acs-lab.eonerc.rwth-aachen.de>
2021-03-23 14:16:41 +0100

    Change name of Infiniband server/client to responder/requester

--- cpu/cpu-ib.h
+++ cpu/cpu-ib.h
@@ -3,14 +3,14 @@
 #include <stdbool.h>
 
 int ib_init(int _device_id);
-int ib_connect_server(void *memreg, int mr_id);
-int ib_connect_client(void *memreg, int mr_id, char *server_address);
+int ib_connect_responder(void *memreg, int mr_id);
+int ib_connect_requester(void *memreg, int mr_id, char *server_address);
 void ib_free_memreg(void* memreg, int mr_id, bool gpumemreg);
 void ib_cleanup(void);
 void ib_final_cleanup(void);
 int ib_allocate_memreg(void** mem_address, int memsize, int mr_id, bool gpumemreg);
 int ib_register_memreg(void** mem_address, int memsize, int mr_id);
-int ib_server_recv(void *memptr, int mr_id, int length, bool togpumem);
-int ib_client_send(void *memptr, int mr_id, int length, char *peer_node, bool fromgpumem);
+int ib_responder_recv(void *memptr, int mr_id, int length, bool togpumem);
+int ib_requester_send(void *memptr, int mr_id, int length, char *peer_node, bool fromgpumem);
 
 #endif //_CPU_IB_H_
