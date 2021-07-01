18838b31febe150aff735a7d97469b094fd9e2fc
Philipp Fensch <philipp.fensch@rwth-aachen.de>
2021-07-01 14:58:41 +0200

    standalone server binary: Use prog-num and vers-num from .x-file instead of hash
    Signed-off-by: Philipp Fensch <philipp.fensch@rwth-aachen.de>

--- cpu/server-library.c
+++ cpu/server-library.c
@@ -5,6 +5,6 @@
 /* shared object constructor; executes before main and thus hijacks main program */
 void __attribute__ ((constructor)) library_constr(void)
 {
-    cricket_main("/proc/self/exe");
+    cricket_main_hash("/proc/self/exe");
 }
 
