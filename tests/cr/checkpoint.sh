fbc25d360bbbeb89c3b6d962a5f9d1cb5499e2f8
Niklas Eiling <niklas.eiling@eonerc.rwth-aachen.de>
2025-01-02 11:44:45 +0100

    cr: clean up scripts
    Signed-off-by: Niklas Eiling <niklas.eiling@eonerc.rwth-aachen.de>

--- tests/cr/checkpoint.sh
+++ tests/cr/checkpoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# tested with CRIU commit ab288c35cb3ac4988151bcec61d44f54a8052dd7
+
 CUDA_APP=${CRICKET_PATH}/tests/samples/samples-bin/nbody.compressed.sample
 export CUDA_APP_NAME=$(basename nbody)
-CUDA_APP_ARGS="-benchmark -numbodies=25600"
+CUDA_APP_ARGS="-compare -numbodies=12800"
 
 
 CRICKET_CLIENT=${CRICKET_PATH}/cpu/cricket-client.so
@@ -13,6 +15,7 @@ CRIU=criu
 export REMOTE_GPU_ADDRESS=ghost.acs-lab.eonerc.rwth-aachen.de
 export CUDA_VISIBLE_DEVICES=3
 
+# clean up previous runs
 rm -rf /tmp/cricket-ckp/*
 rm -rf criu-ckp/*
 rm -rf ckp/*
@@ -20,23 +23,27 @@ mkdir -p criu-ckp
 sudo killall ${CUDA_APP_NAME}
 sudo killall ${CRICKET_SERVER_BIN}
 
+# start cricket server
 ${CRICKET_SERVER}&
 server_pid=$!
 echo "server pid:" $server_pid
+
+# wait until cricket server is up
 sleep 1
 
+# start app
 LD_PRELOAD=${CRICKET_CLIENT} ${CUDA_APP} ${CUDA_APP_ARGS}&
 client_pid=$!
 echo "client pid:" $client_pid
 
-sleep 2
-#sleep 2.95
+# wait until app is doing something interesting
+sleep 1
 
+# create the checkpoint by sending USR1 to the cricket server and dumping the client using CRIU
 kill -s USR1 ${server_pid} && \
 echo $client_pid > criu-ckp/client_pid && \
 sudo ${CRIU} dump -vvvvvv -t $client_pid --tcp-established --shell-job --images-dir ./criu-ckp -W ./criu-ckp -o dump.log
 
+# wait until the checkpoint is done
 sleep 8
-
-#kill $server_pid
 
--- tests/cr/criu-restore-hook.sh
+++ tests/cr/criu-restore-hook.sh
@@ -3,7 +3,6 @@
 case "$CRTOOLS_SCRIPT_ACTION" in
 	"pre-resume" )
         client_pid=$(cat client_pid)
-        #client_pid=$(pgrep matrixMul)
         echo "sending USR1 to $CUDA_APP_NAME with pid $client_pid"
         kill -s USR1 $client_pid
 		exit $?
